d79a5b9f01f67800a2e30b00d67b62a92f844643
Constantino Barreto <pescaalma@gmail.com>
2023-09-24 17:33:20 -0300

    Desabilitando testes unitários

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -2,6 +2,7 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -10,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "ENV",matches = "DEV")
     public void deveEsperarENaoDarTimeout(){
        // Assertions.assertTimeout(Duration.ofSeconds(1),()->SimuladorEspera.esperar(Duration.ofSeconds(10)));
        //ele espera ate o final do teste para verificar a quantidade de tempo esta demorando;
         Assertions.assertTimeoutPreemptively (Duration.ofSeconds(1),()->SimuladorEspera.esperar(Duration.ofSeconds(10)));
+      //  Assertions.assertTrue("PROD".equals(System.getenv("ENV")),()->"Abortando");
         //apos 1 segundo como foi definido ele ja finaliza o teste
     }
 }
